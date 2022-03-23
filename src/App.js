152bf45acc1480f967065e0e622385e602317e89
MarucoMass <mariomass23@gmail.com>
2022-03-23 16:33:06 -0300

    Creacion, carpeta, NavBar.js y cambios en el estilo

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <div className='App-header-container'>
-              <div className='App-header-title'><h1>PedalStore</h1></div>
-            
-              <ul className='App-header-lista'>
-                <li>Distorsiones</li>
-                <li>Modulaciones</li>
-                <li>Delays & Reverbs</li>
-                <li>EQs</li>
-                <li>Compresores</li>
-              </ul>
-          </div>
-      </header>
-    </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
   );
 }
 
---
+++ src/carpeta/NavBar.js
@@ -0,0 +1,23 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <div className='App-header-container'>
+              <div className='App-header-title'><h1><span>Pedal</span>Store</h1></div>
+            
+              <ul className='App-header-lista'>
+                <li>Distorsiones</li>
+                <li>Modulaciones</li>
+                <li>Delays & Reverbs</li>
+                <li>EQs</li>
+                <li>Compresores</li>
+              </ul>
+          </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
