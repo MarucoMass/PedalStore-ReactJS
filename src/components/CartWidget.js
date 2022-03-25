7507af69d4a19def0756fe72d86e5f02b86748c4
MarucoMass <mariomass23@gmail.com>
2022-03-25 19:13:38 -0300

    Cambios en el estilo

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -3,11 +3,11 @@ const CartWidget = () => {
     return(
         <>
             <div className='CartWidget-Box'>
-                <span>Log In</span>
+                <span>Log-In</span>
                 <span>Register</span>
             <div className='img-Box'>
                 <img src={Cart} alt='cart' className='carrito'></img>
-                <div className='number'>6</div>
+                <div className='number'>7</div>
             </div>
             </div>
       
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,40 +2,43 @@ import CartWidget from './CartWidget';
 import Logo from './pedal-svgrepo-com.svg';
 const NavBar = () => {
   return (
-      <>
-      <header className='App-header'>
-          <div className='App-header-contenedor'>
-                <div className='App-header-title'>
-                  <a href='#'>
-                  <span>Pedal</span>
-                   Sh<img src={Logo} alt='logo' className='App-header-logo'></img>p
-                  </a>
-                </div>
-              <div>
-                <ul className='App-header-lista'>
-                  <li><a className='active' href='#'>Home</a></li>
-                  <li><a href='#'>Quienes somos</a></li>
-                  <li className='BoxSubmenu'><a href='#'>Pedales</a>
-                    <ul className="App-header-submenu" aria-labelledby="navbarDropdownMenuLink">
-                      <li><a href="#">Distorsiones</a></li>
-                      <li><a href="#">Modulaciones</a></li>
-                      <li><a href="#">Delays</a></li>
-                      <li><a href="#">Reverbs</a></li>
-                      <li><a href="#">EQs</a></li>
-                      <li><a href="#">Compresores</a></li>
-                      <li><a href="#">Multiefectos</a></li>
-                      </ul>
-                  </li>
-                  <li><a href='#'>Contacto</a></li>
-                    </ul>
+    <>
+    <header className='App-header'>
+        <div className='App-header-contenedor'>
+              <div className='App-header-title'>
+                <a href='#'>
+                <span>Pedal</span>
+                 Sh<img src={Logo} alt='logo' className='App-header-logo'></img>p
+                </a>
               </div>
-          </div>
-              <CartWidget />
-      </header> 
+
+              
+              <ul className='App-header-lista'>
+                <li><a className='active' href='#'>Home</a></li>
+                <li><a href='#'>Quienes somos</a></li>
+                <li className='BoxSubmenu'><a href='#'>Pedales</a>
+                  <ul className="App-header-submenu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a href="#">Distorsiones</a></li>
+                    <li><a href="#">Modulaciones</a></li>
+                    <li><a href="#">Delays</a></li>
+                    <li><a href="#">Reverbs</a></li>
+                    <li><a href="#">EQs</a></li>
+                    <li><a href="#">Compresores</a></li>
+                    <li><a href="#">Multiefectos</a></li>
+                    </ul>
+                </li>
+                <li><a href='#'>Contacto</a></li>
+                  </ul>
+        </div>
+            <CartWidget />
+            <div className='navBurguer'><div className='burguer'></div></div>
+    </header> 
       </>
   // <nav className="navbar navbar-expand-lg App-header">
   //   <div className="container-fluid">
-  //     <a className="navbar-brand App-header-title" href="#">Pedal<span>Store</span></a>
+  //     <a className="App-header-title" href="#"><span>Pedal</span>
+  //       Sh<img src={Logo} alt='logo' className='App-header-logo'></img>p
+  //     </a>
   //     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
   //       <span className="navbar-toggler-icon"></span>
   //     </button>
@@ -58,10 +61,12 @@ const NavBar = () => {
   //             <li><a className="dropdown-item listaPedales" href="#">Reverbs</a></li>
   //             <li><a className="dropdown-item listaPedales" href="#">EQs</a></li>
   //             <li><a className="dropdown-item listaPedales" href="#">Compresores</a></li>
+  //             <li><a className="dropdown-item listaPedales" href="#">Multiefectos</a></li>
   //           </ul>
   //         </li>
   //       </ul>
   //     </div>
+  //     <CartWidget />
   //   </div>
   // </nav>
   );
